e1eddf1c40d003a74ca8a5ddee54b12877ecc16e
moqimoqidea <moqimoqidea@gmail.com>
2024-06-13 12:03:49 +0800

    Test +1

---
+++ rill-flow-common/src/main/java/com/weibo/rill/flow/common/test/SortTest.java
@@ -0,0 +1,13 @@
+package com.weibo.rill.flow.common.test;
+
+/**
+ * @author moqi
+ * On 2024/6/13 12:03
+ */
+public class SortTest {
+
+    public static void bubbleSort(int[] array) {
+
+    }
+
+}
--- rill-flow-common/src/main/java/com/weibo/rill/flow/common/test/WecodeJavaTest.java
+++ rill-flow-common/src/main/java/com/weibo/rill/flow/common/test/WecodeJavaTest.java
@@ -1,4 +1,4 @@
-package com.weibo.rill.flow.common.util;
+package com.weibo.rill.flow.common.test;
 
 import java.util.ArrayList;
 import java.util.List;
